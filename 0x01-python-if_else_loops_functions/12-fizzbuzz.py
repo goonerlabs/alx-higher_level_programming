37a5b74f83b3888193da61fdf51e9329bbe1eeca
goonerlabs <goonerlabs@gmail.com>
2023-08-09 05:17:50 +0100

    complete task

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            str = "FizzBuzz"
+        elif i % 3 == 0:
+            str = "Fizz"
+        elif i % 5 == 0:
+            str = "Buzz"
+        else:
+            str = i
+        print(f"{str} ", end="")
