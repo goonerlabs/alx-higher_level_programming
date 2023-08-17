eb1041db6003d85ab388962c564572611d40bb24
goonerlabs <goonerlabs@gmail.com>
2023-08-17 05:29:30 +0100

    complete task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    res = None
+    max = 0
+    if a_dictionary:
+        for k, v in a_dictionary.items():
+            if v > max:
+                max = v
+                res = k
+    return (res)
