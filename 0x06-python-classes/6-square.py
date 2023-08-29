01b0fbfcff515fdcb9ccdb2c5ec645fc54cfd119
goonerlabs <goonerlabs@gmail.com>
2023-08-29 23:33:13 +0100

    test change made

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -70,8 +70,8 @@ class Square:
             for i in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                print(" " * self.__position[0], end="")
-
+                for x in range(self.__position[0]):
+                    print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
