e296942a75801e6881d82ae2d8847f4f52158602
goonerlabs <goonerlabs@gmail.com>
2023-08-30 00:45:32 +0100

    fix pycodstyle error

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -80,7 +80,7 @@ class Square:
 
     def __str__(self):
         """Returns a string representation of a square"""
-        
+
         if self.__size != 0:
             for i in range(self.__position[1]):
                 print()
