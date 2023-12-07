e6343000a3fba472e06d4e8122a2c7280b047a19
goonerlabs <goonerlabs@gmail.com>
2023-12-07 20:15:25 +0100

    complete task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function that reads a file"""
+
+
+def read_file(filename=""):
+    """reads a file and prints the contents to stdout
+    ARGS:
+    filename: (str) the name of the file to read
+    """
+
+    with open(filename) as f:
+        for line in f:
+            print(line, end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+# alx-higher_level_programming
+Tasks for the python file I/O project
